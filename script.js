82df4e69016849904816401dfe10c3120c9861b2
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-20 15:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const radius = prompt();
+let x = Math.PI;
+let area = (x*radius*radius).toFixed(2);
+alert(`The area of the circle with radius ${radius} is ${area}`);
